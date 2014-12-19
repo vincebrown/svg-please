f172180fd3fc0c73af6e1f720d172826b6020c14
vincebrown <vince@vincebrown.me>
2014-12-19 14:43:31 -0800

    Basic Config and Nav Styles
    Basic Configuration and Styling of the main nav

---
+++ gruntfile.js
@@ -0,0 +1,152 @@
+var timer = require("grunt-timer");
+module.exports = function(grunt) {
+  timer.init(grunt, { deferLogs: true, friendlyTime: true, color: "blue" });
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+  // ===========================================================================
+  // CONFIGURE GRUNT ===========================================================
+  // ===========================================================================
+  grunt.initConfig({
+
+    // get the configuration info from package.json ----------------------------
+    // this way we can use things like name and version (pkg.name)
+    pkg: grunt.file.readJSON('package.json'),
+
+    // configure jshint to validate js files -----------------------------------
+    jshint: {
+      options: {
+        reporter: require('jshint-stylish')
+      },
+      all: ['Gruntfile.js', 'src/**/*.js']
+    },
+
+    // configure uglify to minify js files -------------------------------------
+    uglify: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'dist/js/main.min.js': 'src/js/main.js'
+        }
+      }
+    },
+    
+    // compile less stylesheets to css -----------------------------------------
+    sass: {
+      build: {
+        options: {
+          lineNumber: false,
+          style: 'expanded',
+          require: 'susy'
+        },
+        files: {
+          'src/css/style.css': 'sass/style.scss'
+        }
+      }
+    },
+
+    autoprefixer: {
+      build: {
+        expand: true,
+        cwd: 'src/css',
+        src: [ 'style.css' ],
+        dest: 'src/css'
+      }
+    },
+    // configure cssmin to minify css files ------------------------------------
+    cssmin: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'dist/css/style.min.css': 'src/css/style.css'
+        }
+      }
+    },
+
+    // For common reusable svg Icons
+    svgstore: {
+      options: {
+        prefix: 'shape-',
+        svg: {
+          viewBox: '0 0 50 50',
+          style: "display:none;"
+        }
+      },
+      default: {
+        files: {
+          "includes/svg-defs.svg": ["src/**/*.svg"]
+        }
+      }
+    },
+    imagemin: {
+      png: {
+        options: {
+          optimizationLevel: 7
+        },
+        files: [
+        {
+          // Set to true to enable the following options…
+          expand: true,
+          // cwd is 'current working directory'
+          cwd: 'src/',
+          src: ['**/*.png'],
+          // Could also match cwd line above. i.e. project-directory/img/
+          dest: 'dist/images/',
+          ext: '.png'
+        }
+        ]
+      },
+      jpg: {
+        options: {
+          progressive: true
+        },
+        files: [
+        {
+          // Set to true to enable the following options…
+          expand: true,
+          // cwd is 'current working directory'
+          cwd: 'src/',
+          src: ['**/*.jpg'],
+          // Could also match cwd. i.e. project-directory/img/
+          dest: 'dist/images/',
+          ext: '.jpg'
+        }
+        ]
+      }
+    },
+
+    // configure watch to auto update ------------------------------------------
+    watch: {
+      options: {
+        livereload: true,
+        port: 80
+      },
+      stylesheets: {
+        files: 'sass/**/**/*.scss',
+        tasks: ['sass','autoprefixer','cssmin']
+      },
+      scripts: {
+        files: 'src/**/*.js',
+        tasks: ['jshint', 'uglify']
+      },
+      svg:{
+        files: ['src/**/*.svg'],
+        tasks: ['svgstore']
+      },
+      images: {
+        files: ['src/**/*.{png,jpg,gif}'],
+        tasks: ['imagemin']
+      },
+    }
+
+  });
+
+
+  // ===========================================================================
+  // CREATE TASKS ==============================================================
+  // ===========================================================================
+  grunt.registerTask('default', ['jshint', 'uglify', 'sass','autoprefixer',  'cssmin', 'imagemin']);
+  grunt.registerTask('svg', ['svgstore']);
+};
---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<?php require_once('includes/config.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Svg Please</title>
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="dist/css/style.min.css">
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+</head>
+<body>
+  
+    <header>
+      <div class="logo">
+        <a href="<?php echo BASE_URL;?>"><img src="<?php echo BASE_URL;?>dist/svg/svg-please-logo.svg" alt="Svg Please"></a>
+      </div>
+      <a href="#" class="btn-contrib">VIEW CONTRIBUTORS</a>
+      <nav class="main-nav">
+      <a href="#" class="main-nav__link--articles">Articles</a>
+        <a href="#" class="main-nav__link--books">Books</a>
+        <a href="#" class="main-nav__link--tutorials">Tutorials</a>
+        <a href="#" class="main-nav__link--podcasts">Podcasts</a>
+        <a href="#" class="main-nav__link--talks">Talks</a>
+        <a href="#" class="main-nav__link--tools">Tools</a>
+      </nav>
+    </header>
+    <div class="site-wrapper">
