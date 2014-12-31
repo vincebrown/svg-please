2ea706cf5c001ad81acbfb88e087b2fb90c914e3
Vince Brown <vince@vincebrown.me>
2014-12-30 17:39:39 -0800

    Sample Config Added
    Sample Configuration file

---
+++ includes/sample-config.php
@@ -0,0 +1,16 @@
+<?php
+    // Fill in this config file with proper credentials and resave as config.php
+
+    // these two constants are used to create root-relative web addresses
+    // and absolute server paths throughout all the code
+	define("BASE_URL","/svg-please/");
+	define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/svg-please/");
+
+// DB Credentials
+	define("DB_HOST","localhost");
+	define("DB_NAME","db_name");
+	define("DB_PORT","3306"); // default: 3306
+  define("DB_USER","db_user");
+  define("DB_PASS","db_password");	
+ //  define("DB_USER","root");
+	// define("DB_PASS","root");
